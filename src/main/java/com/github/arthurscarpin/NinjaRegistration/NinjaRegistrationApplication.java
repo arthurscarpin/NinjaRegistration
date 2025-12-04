347a043159666d7cf9d519724055464c0a317cee
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-04 01:22:52 -0300

    first commit

---
+++ src/main/java/com/github/arthurscarpin/NinjaRegistration/NinjaRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.github.arthurscarpin.NinjaRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/arthurscarpin/NinjaRegistration/NinjaRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arthurscarpin.NinjaRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
