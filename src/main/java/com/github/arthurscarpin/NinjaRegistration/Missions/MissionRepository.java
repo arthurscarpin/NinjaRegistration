60f199324bb3a3a0541f12e6ac70200971175783
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-08 15:35:22 -0300

    fix: Corrects the order of parameters in the JPA Repository

--- src/main/java/com/github/arthurscarpin/NinjaRegistration/Missions/MissionRepository.java
+++ src/main/java/com/github/arthurscarpin/NinjaRegistration/Missions/MissionRepository.java
@@ -2,5 +2,5 @@ package com.github.arthurscarpin.NinjaRegistration.Missions;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionRepository extends JpaRepository<Long, MissionModel> {
+public interface MissionRepository extends JpaRepository<MissionModel, Long> {
 }
--- src/main/java/com/github/arthurscarpin/NinjaRegistration/Ninjas/NinjaRepository.java
+++ src/main/java/com/github/arthurscarpin/NinjaRegistration/Ninjas/NinjaRepository.java
@@ -2,5 +2,5 @@ package com.github.arthurscarpin.NinjaRegistration.Ninjas;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NinjaRepository extends JpaRepository<Long, NinjaModel> {
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
