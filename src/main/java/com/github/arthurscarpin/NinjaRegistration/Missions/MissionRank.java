e152b1bce2f87a5abf6b9cc3ea2eaa3bfb0ff3b8
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-09 15:27:46 -0300

    fix: Modify the names of the MissionRank enum

--- src/main/java/com/github/arthurscarpin/NinjaRegistration/Missions/MissionRank.java
+++ src/main/java/com/github/arthurscarpin/NinjaRegistration/Missions/MissionRank.java
@@ -1,9 +1,9 @@
 package com.github.arthurscarpin.NinjaRegistration.Missions;
 
 public enum MissionRank {
-    RANK_S,
-    RANK_A,
-    RANK_B,
-    RANK_C,
-    RANK_D
+    S,
+    A,
+    B,
+    C,
+    D
 }
