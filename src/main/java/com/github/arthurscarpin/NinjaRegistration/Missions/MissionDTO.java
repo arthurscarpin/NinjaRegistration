349434bb1c2973ccdbf5379e3825c42ec7a4e333
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-11 19:38:45 -0300

    fix: Implement the DTO and the mapper for the Mission entity

--- src/main/java/com/github/arthurscarpin/NinjaRegistration/Missions/MissionDTO.java
+++ src/main/java/com/github/arthurscarpin/NinjaRegistration/Missions/MissionDTO.java
@@ -1,4 +1,22 @@
 package com.github.arthurscarpin.NinjaRegistration.Missions;
 
+import com.github.arthurscarpin.NinjaRegistration.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissionDTO {
+
+    private Long id;
+
+    private String name;
+
+    private MissionRank rank;
+
+    private List<NinjaModel> ninja;
 }
