3617a2ad88bfbd97bd714ba5d7fe532289b623df
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2025-12-11 19:19:57 -0300

    chore: Include the DTO version of the Entity and implement the manual mapping in NinjaMapper between the entity and the DTO

--- src/main/java/com/github/arthurscarpin/NinjaRegistration/Ninjas/NinjaDTO.java
+++ src/main/java/com/github/arthurscarpin/NinjaRegistration/Ninjas/NinjaDTO.java
@@ -1,4 +1,26 @@
 package com.github.arthurscarpin.NinjaRegistration.Ninjas;
 
+import com.github.arthurscarpin.NinjaRegistration.Missions.MissionModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NinjaDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String imageUrl;
+
+    private int age;
+
+    private MissionModel missions;
+
+    private NinjaRank rank;
 }
